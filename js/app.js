b853062b6cd020f22364ed78530f94aaba8fd974
BehelitFam <mriodine@gmail.com>
2018-10-18 12:31:21 -0700

    started data filter

--- js/app.js
+++ js/app.js
@@ -5,7 +5,6 @@ const animalDataSets = [];
 let animalsOnPage = [];
 const allKeywords = [];
 const template = Handlebars.compile($('#card-template').html());
-const js
 
 function Animal (animal) {
   this.url = animal.image_url;
@@ -21,6 +20,12 @@ Animal.prototype.render = function() {
   $('#cards').append(template(this));
 }
 
+function addKeyword(keyword) {
+  if (!allKeywords.includes(keyword)) {
+    allKeywords.push(keyword);
+  }
+}
+
 function filterAnimals(keyword) {
   const chosenAnimals = [];
   animalsOnPage.forEach(animal => {
@@ -31,10 +36,11 @@ function filterAnimals(keyword) {
   return chosenAnimals;
 }
 
-function addKeyword(keyword) {
-  if (!allKeywords.includes(keyword)) {
-    allKeywords.push(keyword);
-  }
+function renderAnimals(animals) {
+  $('#cards').empty();
+  animals.forEach(animal => {
+    animal.render();
+  });
 }
 
 function renderFilter() {
@@ -48,29 +54,32 @@ function renderFilter() {
       renderAnimals(filterAnimals(this.value));
     }
   });
+  jsonDataLinks.forEach((link, index) => {
+    $('#pageButtons').append(`<button type="button" value=${link}>Animal Set ${index}</button>`);
+  });
+  $('#pageButtons').on('click', event => {
+    animalsOnPage = event.target.value;
+    renderAnimals(animalsOnPage);
+  });
+
 }
 
 function loadAnimals() {
   jsonDataLinks.forEach(dataLink => {
     $.get(dataLink).then(data => {
+      console.log(dataLink);
       const animalSet = [];
       data.forEach(animal => {
         animalSet.push(new Animal(animal));
       });
+      console.log(animalSet);
       animalDataSets.push(animalSet);
+      console.log(animalDataSets);
     });
   }).then(() => {
     animalsOnPage = animalDataSets[0];
-    renderFilter();
-    renderDataButtons();
+    //renderFilter();
     renderAnimals(animalsOnPage);
-  });
-}
-
-function renderAnimals(animals) {
-  $('#cards').empty();
-  animals.forEach(animal => {
-    animal.render();
   });
 }
 
